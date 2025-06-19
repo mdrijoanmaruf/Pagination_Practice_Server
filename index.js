d1914877502126ac1129cf0e356f3f64e3185859
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-06-20 05:03:16 +0600

    Added API for fetch total numhber of products

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-console.log(process.env.DB_USER, process.env.DB_PASS);
-
-
-
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0ykpaho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
@@ -35,6 +31,14 @@ async function run() {
     app.get('/products', async(req, res) => {
         const result = await productCollection.find().toArray();
         res.send(result);
+    })
+
+
+
+    // Get Total Products number
+    app.get('/productsCount' , async(req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+    res.send({count})
     })
 
     // Send a ping to confirm a successful connection
