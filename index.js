335531e42b08cfb171d060744501ecaac326dcc5
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-06-21 03:40:14 +0600

    API added for fetch data by ides

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,7 +8,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const {
+  MongoClient,
+  ServerApiVersion,
+  ConnectionPoolMonitoringEvent,
+  ObjectId,
+} = require("mongodb");
 
 const uri = `mongodb+srv://libraryManagement:V64snpiDvEnF6Fqk@cluster0.0ykpaho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -17,37 +22,53 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
-    const productCollection = client.db('emaJohnDB').collection('products');
+    const productCollection = client.db("emaJohnDB").collection("products");
 
-    app.get('/products', async(req, res) => {
-      console.log("Pagination Query : " , req.query);
-      const page = parseInt(req.query.page)
-      const size = parseInt(req.query.size)
-        const result = await productCollection.find()
-        .skip(page*size)
+    app.get("/products", async (req, res) => {
+      // console.log("Pagination Query : " , req.query);
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const result = await productCollection
+        .find()
+        .skip(page * size)
         .limit(size)
         .toArray();
-        res.send(result);
-    })
+      res.send(result);
+    });
 
+    app.post("/productsByIds", async (req, res) => {
+      const ids = req.body;
 
+      const idsWithObjectID = ids.map((id) => new ObjectId(id));
+
+      const query = {
+        _id: {
+          $in: idsWithObjectID,
+        },
+      };
+      const result = productCollection.find(query)
+      console.log(idsWithObjectID);
+      res.send(result);
+    });
 
     // Get Total Products number
-    app.get('/productsCount' , async(req, res) => {
+    app.get("/productsCount", async (req, res) => {
       const count = await productCollection.estimatedDocumentCount();
-    res.send({count})
-    })
+      res.send({ count });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -55,11 +76,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("john is busy shopping");
+});
 
-app.get('/', (req, res) =>{
-    res.send('john is busy shopping')
-})
-
-app.listen(port, () =>{
-    console.log(`ema john server is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`ema john server is running on port: ${port}`);
+});
